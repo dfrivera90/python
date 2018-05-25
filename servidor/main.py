0f12be8b52146f2c8d188bafd9da609b1a6e3bf9
Daniel Rivera <dfrivera_90@hotmail.com>
2018-05-24 21:52:43 -0500

    Jinja 2

--- servidor/main.py
+++ servidor/main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hola_mundo():
-    return 'Oiga! La amo mucho!'
+    return render_template("agenda.html")
 
 if __name__ == '__main__':
     app.run()
